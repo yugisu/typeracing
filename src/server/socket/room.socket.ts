512c1adabb0b25b256a877ac1ae4e3a28a2cecc3
yugisu <slymahel@gmail.com>
2019-07-10 13:03:43 +0300

    Separate room socket logic from overall

---
+++ src/server/socket/room.socket.ts
@@ -0,0 +1,61 @@
+import { verify } from 'jsonwebtoken';
+
+import { Room } from 'server/controllers/room.controller';
+import { User } from 'shared/types/user.type';
+
+const rooms: Map<string, Room> = new Map();
+
+const findRoom = (username: string) => {
+  for (const room of rooms.values()) {
+    if (room.state.progresses[username] !== undefined || !room.state.active) {
+      return room;
+    }
+  }
+
+  return null;
+};
+
+const createRoom = (io: SocketIO.Server): Room => new Room(io);
+
+const recordRoom = (roomMap: Map<string, Room>) => (room: Room): Room => {
+  roomMap.set(room.state.name, room);
+  return room;
+};
+
+export const initRoomSocket = (io: SocketIO.Server) => (socket: SocketIO.Socket) => {
+  try {
+    if (!(socket.handshake.query && socket.handshake.query.token)) {
+      throw 'AuthError: No token sent';
+    }
+    const user = verify(
+      socket.handshake.query.token,
+      process.env.SECRET_KEY || 'secret'
+    ) as User | string;
+
+    if (typeof user === 'string' || !user.hasOwnProperty('login'))
+      throw `AuthError: Invalid token: ${user}`;
+
+    const username = user.login;
+    const room = findRoom(username) || recordRoom(rooms)(createRoom(io));
+
+    socket.join(room.state.name);
+
+    socket.emit('authSuccess', username, room.state);
+    room.notifier.emitLocal('playerJoined', username);
+
+    // Add event listeners if success
+    socket.on('playerProgress', (username: string, progress: number) => {
+      room.notifier.emit('playerProgress', username, progress);
+    });
+
+    socket.on('disconnect', () => {
+      room.notifier.emitLocal('playerLeft', username, () =>
+        rooms.delete(room.state.name)
+      );
+    });
+  } catch (error) {
+    console.error(error);
+    socket.emit('authFail');
+    socket.disconnect(true);
+  }
+};
